d6a499ed7a6dbe90425bc5358b414c16d082ea47
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2024-11-22 20:50:06 +0530

    Create Animal Matching Game project

---
+++ mygame/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='header'>
+        <h1>ANIMAL MATCHING GAME</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
