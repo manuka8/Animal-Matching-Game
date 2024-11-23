3ce8dd854a5ddf52d50c689f792c652f9dc6a079
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2024-11-23 09:29:47 +0530

    Modify App.js

--- mygame/src/App.js
+++ mygame/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import AnimalName from './components/AnimalName';
+import SelectAnimal from './components/SelectAnimal';
 
 function App() {
   return (
     <div className="App">
       <div className='header'>
-        <h1>ANIMAL MATCHING GAME</h1>
+        <h2>ANIMAL MATCHING GAME</h2>
+      </div>
+      <div className='body-container'>
+        <AnimalName />
+      </div>
+      <div>
+        <SelectAnimal/>
       </div>
     </div>
   );
