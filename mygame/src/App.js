7b91f12606233c234658fa66e36315d11a88b7dc
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2024-11-23 22:33:47 +0530

    Creating relationships between childrens

--- mygame/src/App.js
+++ mygame/src/App.js
@@ -1,18 +1,34 @@
+import { useState } from 'react';
 import './App.css';
 import AnimalName from './components/AnimalName';
 import SelectAnimal from './components/SelectAnimal';
+import Result from './components/Result';
 
 function App() {
+  const [img, setImg] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleImg = (selectedImg) => {
+    setImg(selectedImg); // Update image in state
+  };
+
+  const handleResult = (isSame) => {
+    setResult(isSame ? "Correct Match!" : "Wrong Match!"); // Update result
+  };
+
   return (
     <div className="App">
-      <div className='header'>
+      <div className="header">
         <h2>ANIMAL MATCHING GAME</h2>
       </div>
-      <div className='body-container'>
-        <AnimalName />
+      <div className="body-container">
+        <AnimalName animalImg={handleImg} />
       </div>
       <div>
-        <SelectAnimal/>
+        <SelectAnimal receivedAnimalImg={img} result={handleResult} />
+      </div>
+      <div>
+        <Result receivedResult={result} />
       </div>
     </div>
   );
