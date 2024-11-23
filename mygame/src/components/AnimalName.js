73460029038049eebb4dc7d793c27b56f8528b48
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2024-11-23 22:37:29 +0530

    Show random animal name

--- mygame/src/components/AnimalName.js
+++ mygame/src/components/AnimalName.js
@@ -3,7 +3,13 @@ import { animals } from "../AnimalsDb";
 
 const AnimalName = ({ animalImg }) => {
   const randnum = Math.floor(Math.random() * animals.length);
-  
+  animalImg(animals[randnum].img);
+  return (
+    <div>
+      <h1>Animal Name</h1>
+      <p>{animals[randnum].name}</p>
+    </div>
+  );
 };
 
 export default AnimalName;
